649e2bcf154a09d64a32e73d721cee2a728f1fd1
ersaistary <ersaistary31@gmail.com>
2025-06-15 22:29:54 +0700

    db

---
+++ laundry_test.sql
@@ -0,0 +1,316 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2025 at 05:28 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laundry_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `phone` varchar(13) NOT NULL,
+  `address` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `customer_name`, `phone`, `address`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Raka', '1234', 'Bandung', '2025-06-15 06:32:14', '2025-06-15 07:40:56', NULL),
+(2, 'Tina', '12345678', 'Tokyo', '2025-06-15 07:40:48', '2025-06-15 07:40:59', '2025-06-15 14:40:59'),
+(3, 'Mawar', '1234', 'Beijing', '2025-06-15 12:07:35', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `id` int(11) NOT NULL,
+  `level_name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `level`
+--
+
+INSERT INTO `level` (`id`, `level_name`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Adminstrator', '2025-06-15 04:20:52', NULL, NULL),
+(2, 'Operator', '2025-06-15 04:20:52', NULL, NULL),
+(3, 'Pimpinan', '2025-06-15 04:20:52', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trans_laundry_pickup`
+--
+
+CREATE TABLE `trans_laundry_pickup` (
+  `id` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `pickup_date` datetime NOT NULL,
+  `notes` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trans_order`
+--
+
+CREATE TABLE `trans_order` (
+  `id` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `order_code` varchar(50) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_end_date` date DEFAULT NULL,
+  `order_status` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL,
+  `order_pay` int(11) NOT NULL,
+  `order_change` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `trans_order`
+--
+
+INSERT INTO `trans_order` (`id`, `id_customer`, `order_code`, `order_date`, `order_end_date`, `order_status`, `created_at`, `updated_at`, `deleted_at`, `order_pay`, `order_change`, `total`) VALUES
+(12, 1, 'ord1', '2025-06-15', NULL, 0, '2025-06-15 12:06:47', NULL, NULL, 50000, 45000, 5000),
+(13, 3, 'ord2', '2025-06-15', NULL, 0, '2025-06-15 12:07:55', NULL, NULL, 50000, 15000, 35000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trans_order_detail`
+--
+
+CREATE TABLE `trans_order_detail` (
+  `id` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_service` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `subtotal` double(10,2) NOT NULL,
+  `notes` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updates_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_of_service`
+--
+
+CREATE TABLE `type_of_service` (
+  `id` int(11) NOT NULL,
+  `service_name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `type_of_service`
+--
+
+INSERT INTO `type_of_service` (`id`, `service_name`, `price`, `description`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Cuci Gosok', 5000000, 'Cuci Gosok', '2025-06-15 08:27:36', NULL, NULL),
+(2, 'Hanya Cuci', 4500000, 'Cuci saja', '2025-06-15 08:27:36', NULL, NULL),
+(3, 'Gosok', 5000000, 'Gosok saja', '2025-06-15 08:27:36', NULL, NULL),
+(4, 'Laundry Besar', 7000000, 'Laundry besar', '2025-06-15 08:27:36', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `id_level` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `id_level`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Admin 1', 'admin@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2025-06-15 04:22:39', NULL),
+(2, 2, 'Operator 1', 'operator@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2025-06-15 04:22:39', NULL),
+(3, 3, 'Pimpinan 1', 'pimpinan@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', '2025-06-15 04:22:39', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trans_laundry_pickup`
+--
+ALTER TABLE `trans_laundry_pickup`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `trans_laundry_pickup(id_order)-to-trans_order(id)` (`id_order`),
+  ADD KEY `trans_laundry_pickup(id_customer)-to-customer(id)` (`id_customer`);
+
+--
+-- Indexes for table `trans_order`
+--
+ALTER TABLE `trans_order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `trans_order(id_customer)-to-customer(id)` (`id_customer`);
+
+--
+-- Indexes for table `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `trans_order_detail(id_service)-to-type_of_service(id)` (`id_service`),
+  ADD KEY `trans_order_detail(id_service)-to-trans_order(id)` (`id_order`);
+
+--
+-- Indexes for table `type_of_service`
+--
+ALTER TABLE `type_of_service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user(idLevel)-to-level(idLevel)` (`id_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `trans_laundry_pickup`
+--
+ALTER TABLE `trans_laundry_pickup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trans_order`
+--
+ALTER TABLE `trans_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type_of_service`
+--
+ALTER TABLE `type_of_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `trans_laundry_pickup`
+--
+ALTER TABLE `trans_laundry_pickup`
+  ADD CONSTRAINT `trans_laundry_pickup(id_customer)-to-customer(id)` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`),
+  ADD CONSTRAINT `trans_laundry_pickup(id_order)-to-trans_order(id)` FOREIGN KEY (`id_order`) REFERENCES `trans_order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `trans_order`
+--
+ALTER TABLE `trans_order`
+  ADD CONSTRAINT `trans_order(id_customer)-to-customer(id)` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  ADD CONSTRAINT `trans_order_detail(id_service)-to-trans_order(id)` FOREIGN KEY (`id_order`) REFERENCES `trans_order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `trans_order_detail(id_service)-to-type_of_service(id)` FOREIGN KEY (`id_service`) REFERENCES `type_of_service` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user(idLevel)-to-level(idLevel)` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
